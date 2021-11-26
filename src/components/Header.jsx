1050f64e85c8fcfb49689545d42199f26ba3f54a
JAMChannel <pon244tt@gmail.com>
2021-11-26 16:49:30 +0900

    Reactコンポーネント作成

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export const Header = () => {
+
+  const componentName = () => 'Header';
+
+  return (
+    <div className="test-wrapper">
+      このコンポーネントの名前は{componentName()}です。
+    </div>
+  );
+};
